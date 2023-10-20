aa6da7df007bb4b57bde2b660a3d9b03f8e85f45
Kiriki-Liszt <kirikiaris@gmail.com>
2023-10-21 00:00:02 +0900

    init

---
+++ scripts/func.js
@@ -0,0 +1,73 @@
+
+var sort_selected = "byAdd";
+
+function searchEnter() {
+	if (window.event.keyCode==13) {               
+		populateSection(musicbook, 1);
+	}
+}
+function searchUpdate() {
+	const search_update = document.getElementById("inputsearch");
+	search_update.setAttribute("value", search_update.value);
+}
+
+function sortArtist() {
+	document.getElementById(sort_selected).classList.remove("button-selected");
+	document.getElementById("byArtist").classList.add("button-selected");
+	sort_selected = "byArtist";
+	musicbook = artistOrdered;
+	populateSection(musicbook, 1);
+}
+function sortSong() {
+	document.getElementById(sort_selected).classList.remove("button-selected");
+	document.getElementById("bySong").classList.add("button-selected");
+	sort_selected = "bySong";
+	musicbook = songOrdered;
+	populateSection(musicbook, 1);
+}
+function sortAdded() {
+	document.getElementById(sort_selected).classList.remove("button-selected");
+	document.getElementById("byAdd").classList.add("button-selected");
+	sort_selected = "byAdd";
+	musicbook = addOrdered;
+	populateSection(musicbook, 1);
+}
+
+function new_random() {
+	random_select(musicbook,6);
+}
+
+document.getElementById("openMenu").onclick = function() {
+	var idLeft = document.getElementById("sidebar-id");
+	if (idLeft.classList.contains("sidebar-hide")) {
+		idLeft.classList.remove("sidebar-hide");
+	}
+	else {
+		idLeft.classList.add("sidebar-hide");
+	}
+
+	idLeft = document.getElementById("sidebar-wrapper-id");
+	if (idLeft.classList.contains("sidebar-wrapper-hide")) {
+		idLeft.classList.remove("sidebar-wrapper-hide");
+	}
+	else {
+		idLeft.classList.add("sidebar-wrapper-hide");
+	}
+};        
+
+
+let removeToast;
+
+function toast(string) {
+    const toast = document.getElementById("toast");
+
+    toast.classList.contains("reveal") ?
+        (clearTimeout(removeToast), removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 1000)) :
+        removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 3000)
+    toast.classList.add("reveal"),
+        toast.innerText = string
+}
---
+++ scripts/google-sheet.js
@@ -0,0 +1,364 @@
+var header  = document.querySelector('header');
+var section = document.querySelector('section');
+var genre = document.querySelector('genre');
+var category = document.querySelector('category');
+var random = document.querySelector('random');
+
+let myKey = "1mCzFdijbvXyE-chn2tUGucSK6PByiup4GVn7ecy-W8Q"; // 스프레드시트 KEY
+
+let noCover = `https://cdn.discordapp.com/attachments/1158397408514932746/1160924994839248926/0_.png`;
+
+var musicbook;
+var addOrdered;
+var artistOrdered;
+var songOrdered;
+
+var category_selected;
+var categories;
+let genres = [];
+var genre_selected;
+
+google.charts.load("current", { packages: ["corechart"] }).then(() => {
+	let query = new google.visualization.Query(
+		`https://docs.google.com/spreadsheets/d/${myKey}/gviz/tq?tqx=out:json`
+	);
+
+	query.send((response) => {
+		if (response.isError()) {
+			console.error(
+				"Error in query: " + response.getMessage() + " " + response.getDetailedMessage()
+			);
+			return;
+		}
+
+		let dataTable = response.getDataTable().toJSON(); 
+		let jsonData = JSON.parse(dataTable);
+		console.log(jsonData);
+		// let cols = jsonData.cols.map((col) => col.label); console.log("cols: \n", cols);
+		let cols = ["order", "artist", "song", "genre", "category", "cover_link"];
+		musicbook = jsonData.rows.map((row) => {
+			let newRow;
+			row.c.forEach((obj, index) => {
+				if (obj == null || obj == undefined) return; //빈값이 경우 정지
+				obj[cols[index]] = "f" in obj ? obj["f"] : obj["v"];
+				["f", "v"].forEach((each) => delete obj[each]);
+				
+				if (cols[index] == "genre") {
+					obj.genre = (obj.genre).split("/");
+				}
+				// console.log(JSON.stringify(obj.cover_link));
+				if (cols[index] == "cover_link") {
+					if (obj.cover_link == null) {
+						obj.cover_link = noCover;
+					}
+					else if ((obj.cover_link).substring(0,4) != "http") {
+						obj.cover_link = "https://img.youtube.com/vi/" + obj.cover_link + "/maxresdefault.jpg";
+						
+					}
+					
+
+				}
+
+				newRow = { ...newRow, ...obj };
+			});
+			return newRow;
+		});
+
+		addOrdered = JSON.parse(JSON.stringify(musicbook));
+
+		musicbook.sort((a, b) => {
+			a = a.song.toLowerCase();
+			b = b.song.toLowerCase();
+			if (a > b) return 1;
+			if (a < b) return -1;
+			return 0;
+		});
+		songOrdered = JSON.parse(JSON.stringify(musicbook));
+		// console.log("Song Ordered\n", songOrdered);
+
+		musicbook.sort((a, b) => {
+			a = a.artist.toLowerCase();
+			b = b.artist.toLowerCase();
+			if (a > b) return 1;
+			if (a < b) return -1;
+			return 0;
+		});
+		artistOrdered = JSON.parse(JSON.stringify(musicbook));
+		// console.log("artist Ordered\n", artistOrdered);
+
+		category_populate(musicbook);
+		genre_populate(musicbook);
+		random_select(musicbook,6);
+
+		category_selected = "";
+		genre_selected = "";
+		sortAdded();
+	});
+});
+
+
+function genre_populate(jsonObj) {
+
+	//categories = Array.from(new Set(jsonObj.map(item => item.genre)));
+
+	
+	// console.log(jsonObj);
+	jsonObj.forEach(
+		(item) => {
+			item.genre.forEach(
+				(gen) => {
+					if (!genres.includes(gen)) {
+						genres.push(gen);
+					}
+				}
+			)
+		}
+	);
+
+	var cateDiv = document.createElement('div');
+	cateDiv.classList.add("genre-select");
+	genre.appendChild(cateDiv);
+
+	for (var i = 0; i < genres.length; i++) {
+		var cateName = document.createElement('button');
+		var cateString = document.createElement('formatted-string');
+
+		cateString.textContent = genres[i];
+		cateString.classList.add("genre-text");
+		cateName.appendChild(cateString);
+
+		cateName.classList.add("genre-button");
+		cateName.classList.add("clickable");
+		cateName.setAttribute("id", "genre-" + i);
+
+		cateName.addEventListener('click', function () {
+			var prev_sel = document.getElementsByClassName("genre-button");
+			if ( this.classList.contains("button-selected") ) {
+				for( var i = 0; i < prev_sel.length; i++ ){
+					prev_sel.item(i).classList.remove("button-selected");
+				}
+				genre_selected = "";
+				populateSection(musicbook, 1);
+			}
+			else {
+				for( var i = 0; i < prev_sel.length; i++ ){
+					prev_sel.item(i).classList.remove("button-selected");
+				}
+				this.classList.add("button-selected");
+				genre_selected = this.textContent;
+				populateSection(musicbook, 1);
+			}
+		});
+
+		cateDiv.appendChild(cateName);
+	}
+}
+
+
+
+function category_populate(jsonObj) {
+
+	categories = Array.from(new Set(jsonObj.map(item => item.category)));
+
+	var cateDiv = document.createElement('div');
+	cateDiv.classList.add("category-select");
+	category.appendChild(cateDiv);
+
+	for (var i = 0; i < categories.length; i++) {
+		var cateName = document.createElement('button');
+		var cateString = document.createElement('formatted-string');
+
+		cateString.textContent = categories[i];
+		cateString.classList.add("category-text");
+		cateName.appendChild(cateString);
+
+		cateName.classList.add("category-button");
+		cateName.classList.add("clickable");
+		cateName.setAttribute("id", "category-" + i);
+
+		cateName.addEventListener('click', function () {
+			var prev_sel = document.getElementsByClassName("category-button");
+			if ( this.classList.contains("button-selected") ) {
+				for( var i = 0; i < prev_sel.length; i++ ){
+					prev_sel.item(i).classList.remove("button-selected");
+				}
+				category_selected = "";
+				populateSection(musicbook, 1);
+			}
+			else {
+				for( var i = 0; i < prev_sel.length; i++ ){
+					prev_sel.item(i).classList.remove("button-selected");
+				}
+				this.classList.add("button-selected");
+				category_selected = this.textContent;
+				populateSection(musicbook, 1);
+			}
+		});
+
+		cateDiv.appendChild(cateName);
+	}
+}
+
+
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
+}
+function random_select(jsonObj, num) {
+
+	var musiclist = jsonObj;
+
+	/* 기존 노래들 클리어 */
+	const myNode = document.getElementById("musicListRandom");
+	while (myNode.lastElementChild) {
+		myNode.removeChild(myNode.lastElementChild);
+	}
+
+	var dup = [];
+	dup[0] = 0;
+	var i = 0;
+
+	for (i; i < num; i = i + 1) {
+
+		var rnd = getRndInteger(1, musiclist.length);
+
+		
+		for (var j = 0; j < i; j = j + 1) { 
+			while (dup[j] == rnd) {
+				rnd = rnd + 1;
+				if (rnd == musiclist.length) { rnd = 1; }
+				j = 0;
+				console.log("Random Colide!");
+			}
+		}
+		dup[i] = rnd; 
+
+		var myDiv = document.createElement('div');
+
+		var coverDiv = document.createElement('div');
+		var coverImg = document.createElement('img');
+
+		var infoDiv = document.createElement('div');
+		var infoSong = document.createElement('formatted-string');
+		var infoArtist = document.createElement('formatted-string');
+
+		myDiv.classList.add("random-song");
+		
+		coverDiv.classList.add("random-cover-div");
+		coverImg.classList.add("random-cover-img");
+		if (musiclist[rnd].cover_link == null) coverImg.src = noCover;
+		else coverImg.src = musiclist[rnd].cover_link;
+
+		infoDiv.classList.add("random-info-div");
+		infoArtist.classList.add("random-artist-name");
+		infoSong.classList.add("random-song-name");
+		infoArtist.textContent = musiclist[rnd].artist;
+		infoSong.textContent = musiclist[rnd].song;
+
+		coverDiv.appendChild(coverImg);
+		infoDiv.appendChild(infoSong);
+		infoDiv.appendChild(infoArtist);
+		myDiv.appendChild(coverDiv);
+		myDiv.appendChild(infoDiv);
+
+		myDiv.classList.add("clickable");
+		myDiv.addEventListener('click', function () {
+			var song = this.childNodes[1].childNodes[0];
+			var artist = this.childNodes[1].childNodes[1];
+			var text = song.textContent + " - " + artist.textContent;
+			window.navigator.clipboard.writeText(text).then(() => {
+				toast("복사완료");
+			});
+		});
+		
+		random.appendChild(myDiv);
+	}
+
+}
+
+function populateSection(jsonObj, direction) {
+
+	var musiclist = jsonObj;
+	/* console.log("populateSection", musiclist); */
+	/* 기존 노래들 클리어 */
+	const myNode = document.getElementById("musicList");
+	while (myNode.lastElementChild) {
+		myNode.removeChild(myNode.lastElementChild);
+	}
+
+	/* 검색 입력창에 들어와있는거 저장 */
+	const search_value = document.getElementById("inputsearch").value;
+
+	var i, end;
+	if (direction == 1) {
+		i = 0;
+		end = musiclist.length;
+	}
+	else {
+		i = musiclist.length - 1;
+		end = -1;
+	}
+
+	for (i; i != end; i = i + direction) {
+		if ( search_value != "" ) {
+			if (musiclist[i].artist.indexOf(search_value)==-1 && 
+				musiclist[i].song.indexOf(search_value)==-1 ) {
+				continue; 
+			}
+		}
+		if ( (category_selected != "") && (musiclist[i].category != category_selected) ) {
+			continue;
+		}
+		var temp_gen= false;
+		musiclist[i].genre.forEach(
+			(item) => {
+				if(item == genre_selected) {
+					temp_gen = true;
+				}
+				
+			}
+		);
+		if ( (genre_selected != "") && !temp_gen ) {
+			continue;
+		}
+
+		var myDiv = document.createElement('div');
+
+		var coverDiv = document.createElement('div');
+		var coverImg = document.createElement('img');
+
+		var infoDiv = document.createElement('div');
+		var infoSong = document.createElement('formatted-string');
+		var infoArtist = document.createElement('formatted-string');
+
+		myDiv.classList.add("song-div");
+		
+		coverDiv.classList.add("album-cover-div");
+		coverImg.classList.add("album-cover-img");
+		if (musiclist[i].cover_link == null) coverImg.src = noCover;
+		else coverImg.src = musiclist[i].cover_link;
+
+		infoDiv.classList.add("info-div");
+		infoArtist.classList.add("artist-name");
+		infoSong.classList.add("song-name");
+		infoArtist.textContent = musiclist[i].artist;
+		infoSong.textContent = musiclist[i].song;
+
+		coverDiv.appendChild(coverImg);
+		infoDiv.appendChild(infoSong);
+		infoDiv.appendChild(infoArtist);
+		myDiv.appendChild(coverDiv);
+		myDiv.appendChild(infoDiv);
+
+		myDiv.classList.add("clickable");
+		myDiv.addEventListener('click', function () {
+			var song = this.childNodes[1].childNodes[0];
+			var artist = this.childNodes[1].childNodes[1];
+			var text = song.textContent + " - " + artist.textContent;
+			window.navigator.clipboard.writeText(text).then(() => {
+				toast("복사완료");
+			});
+		});
+
+		section.appendChild(myDiv);
+	}
+}
